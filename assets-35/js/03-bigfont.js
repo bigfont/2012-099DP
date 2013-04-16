a2fe85c5ef194d0889941c0fd353bec6f94273c9
Shaun Luttin <shaunluttin@gmail.com>
2013-04-16 12:07:23 -0700

    Add a horizontal rule after each section, because it look professional.

--- assets-35/js/03-bigfont.js
+++ assets-35/js/03-bigfont.js
@@ -3,11 +3,23 @@
     'use strict';
     /*global $:false, document:false, window:false */
 
-    $(document).ready(function () {
+    function addHorizontalRuleAfterEachSection()
+    {
+        var horizontalRule = $('<hr/>');
+        $('section').after(horizontalRule);
+    }
 
+    function initializeTheCarousel()
+    {
         $('.carousel').carousel({
             interval: 5000
         });
+    }
+
+    $(document).ready(function () {
+
+        initializeTheCarousel();
+        addHorizontalRuleAfterEachSection();
 
     });
 
