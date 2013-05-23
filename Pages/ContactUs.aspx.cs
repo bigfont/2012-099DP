667c04e5ee02875a4a2a3485df1754c9d9c83042
Shaun Luttin <shaunluttin@gmail.com>
2013-05-23 16:35:07 -0700

    Added Google Analytics, although it doesn't appear to be installed properly. Oh well, it's a niec to have.

--- Pages/ContactUs.aspx.cs
+++ Pages/ContactUs.aspx.cs
@@ -11,20 +11,7 @@ namespace DavesBlasting.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // We're currently sending an email on page load
-            // just to test the system.net.mail in conjunction with SendGrid.
-            SendEmail();
-        }
-        private void SendEmail()
-        {                     
-            // In addition to the web.configuration.system.net.mailSettings, 
-            // the following is the only code needed to send an email with SendGrid.
-            System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
-            message.To.Add("admin@shaunluttin.com");            
-            message.Subject = "Testing the SendGrid Library";
-            message.Body = "Hello World";
-            System.Net.Mail.SmtpClient Smtp = new System.Net.Mail.SmtpClient();
-            Smtp.Send(message);
+
         }
     }
 }
