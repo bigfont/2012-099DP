50fa061230d27c3d25df5eb202e628174facf361
Shaun Luttin <shaunluttin@gmail.com>
2013-07-29 12:59:08 -0700

    Remove the email sending functionality, because we aren't using it and because it adds unnecessary complexity.

--- Site.Master.cs
+++ Site.Master.cs
@@ -8,24 +8,8 @@ using System.Web.UI.WebControls;
 namespace DavesBlasting {
     public partial class Site1 : System.Web.UI.MasterPage {
         protected void Page_Load(object sender, EventArgs e) {
-            // We're currently sending an email on page load
-            // just to test the system.net.mail in conjunction with SendGrid.
-            string ip, subject;
-            ip = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
-            if (string.IsNullOrEmpty(ip))
-                ip = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
-            subject = string.Format("{0} visited DavesBlasting", ip);
-            SendEmail(subject);
-        }
-        private void SendEmail(string subject) {
-            // In addition to the web.configuration.system.net.mailSettings, 
-            // the following is the only code needed to send an email with SendGrid.
-            System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
-            message.To.Add("admin@shaunluttin.com");
-            message.Subject = subject;
-            message.Body = "Hello World";
-            System.Net.Mail.SmtpClient Smtp = new System.Net.Mail.SmtpClient();
-            Smtp.Send(message);
+
+
         }
     }
 }
